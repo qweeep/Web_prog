92df8b147a97428aad349c0cb5e57714ad0a50a3
Lev Krasulin <90341548+qweeep@users.noreply.github.com>
2021-12-22 20:49:12 +0300

    Add files via upload

---
+++ Flask_app/flask_app/flask_app/test.py
@@ -0,0 +1,35 @@
+import requests
+
+
+print('Создание пользователя:')
+response = requests.put(
+    'http://localhost:8080/user/boca',
+    {'password': 'fsdv4w5#'}
+)
+print(response.json())
+
+print('Создание пользователя с таким же паролем:')
+response = requests.put(
+    'http://localhost:8080/user/joca',
+    {'password': 'fsdv4w5#'}
+)
+print(response.json())
+
+print('Попытка создать пользователя с уже существующим именем:')
+response = requests.put(
+    'http://localhost:8080/user/boca',
+    {'password': '3456oijUYr'}
+)
+print(response.json())
+
+print('Получение данных о пользователе:')
+response = requests.get('http://localhost:8080/user/boca')
+print(response.json())
+
+print('Удаление пользователя:')
+response = requests.delete('http://localhost:8080/user/joca')
+print(response.text)
+
+print('Попытка получить информацию о несуществующем пользователе:')
+response = requests.get('http://localhost:8080/user/joca')
+print(response.json())
